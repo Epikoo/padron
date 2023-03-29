ed33858f7aee1636c6e1784602a6b0b882d18dda
Epikoo <ignacioandresdiazromero@gmail.com>
2023-03-29 22:09:15 +0200

    Patron terminado y funcional

--- padron.java
+++ padron.java
@@ -8,6 +8,8 @@ import java.io.FileWriter;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @version 1.0
@@ -23,86 +25,91 @@ public class padron {
         String linea;
         String ruta = "src/Ficheros/padron.txt";
         //Flujo de lectura
-        FileWriter escritor = null;
+        //FileWriter escritor = null;
         BufferedWriter miBufferedWriter = null;
         //Flujo de escritura
         BufferedReader miBufferedReader = null;
-        FileReader lector = null;
+        //FileReader lector = null;
 
-        try {
-            //flujo de esccritura
-            lector = new FileReader(ruta);
-            miBufferedReader = new BufferedReader(lector);
-            //flujo de lectura
-            escritor = new FileWriter(ruta);
-            miBufferedWriter = new BufferedWriter(escritor);
+        do {
+            //menu
+            System.out.println("Padron de Habitantes de Villanueva del Trabuco");
+            System.out.println("==============================================");
+            System.out.println("Elija una opcion : \n1.-Registrar habitante \n2.-Mostrar patron\n3.-Salir\n¿Opcion?");
+            //----------------------VALIDACION DE OPCION--------------------
             do {
-                //menu
-                System.out.println("Padron de Habitantes de Villanueva del Trabuco");
-                System.out.println("==============================================");
-                System.out.println("Elija una opcion : \n1.-Registrar habitante \n2.-Mostrar patron\n3.-Salir\n¿Opcion?");
-                //----------------------VALIDACION DE OPCION--------------------
-                do {
-                    if (!checked) {
-                        System.out.println("Opcion no valida (1-3)");
-                    }
-                    try {
-                        checked = true;
-                        selectmenu = t.nextInt();
-                        if (selectmenu != 1 && selectmenu != 2 && selectmenu != 3) {
-                            checked = false;
-                        }
-                    } catch (InputMismatchException e) {
-                        System.out.println("Debe de ser un numero");
+                if (!checked) {
+                    System.out.println("Opcion no valida (1-3)");
+                }
+                try {
+                    checked = true;
+                    selectmenu = t.nextInt();
+                    if (selectmenu != 1 && selectmenu != 2 && selectmenu != 3) {
                         checked = false;
-                        t.nextLine();
                     }
-                } while (!checked);
-                checked = true;//flag on por si nos vuelve a hacer falta
-                //==================================EMPIEZA EL MENU=====================================
-                switch (selectmenu) {
-                    //=====================CASE1=============================
-                    case 1:
-                        //------------------------------Validacion del Dni-----------------------
-                        do {
-                            System.out.println("Dni del registro");
-                            dni = t.next();
-                            if (dni.length() != 9) {
-                                System.out.println("El dni debe tener el formato 99999999Z");
-                            }
-                        } while (dni.length() != 9);
-                        //----------------------------flujo de lectura y escritura----------------
-                        do {
-                            linea = miBufferedReader.readLine();
-                            lector.read();
-                            if (linea != null) {
-                                escritor.write(linea);
-                                miBufferedWriter.newLine();
-                            }
-                        } while (linea != null);
+                } catch (InputMismatchException e) {
+                    System.out.println("Debe de ser un numero");
+                    checked = false;
+                    t.nextLine();
+                }
+            } while (!checked);
+            checked = true;//flag on por si nos vuelve a hacer falta
+            //==================================EMPIEZA EL MENU=====================================
+            switch (selectmenu) {
+                //=====================CASE1=============================
+                case 1:
+                    //------------------------------Validacion del Dni-----------------------
+                    do {
+                        System.out.println("Dni del registro");
+                        dni = t.next();
+                        if (dni.length() != 9) {
+                            System.out.println("El dni debe tener el formato 99999999Z");
+                        }
+                    } while (dni.length() != 9);
+                    try {
+                        miBufferedWriter = new BufferedWriter(new FileWriter(ruta, true));
                         miBufferedWriter.write(dni);
-                        break;
-                    //======================CASE 2======================
-                    case 2:
-                         linea = miBufferedReader.readLine();
-                        System.out.println(linea);
-                        do {
-                           
+                        miBufferedWriter.newLine();
+                    } catch (IOException ex) {
+                        System.out.println("Error inesperado " + ex.getMessage());
+                    } finally {
+                        if (miBufferedWriter != null) {
+                            try {
+                                miBufferedWriter.close();
+                            } catch (IOException ex) {
+                                System.out.println("Error inesperado " + ex.getMessage());
+                            }
+                        }
+                    }
+                    //----------------------------flujo de lectura y escritura----------------
+                    break;
+                //======================CASE 2======================
+                case 2:
+                {
+                    try {
+                        miBufferedReader=new BufferedReader(new FileReader(ruta));
+                    } catch (FileNotFoundException ex) {
+                        System.out.println("No se encontro el archivo");
+                    }
+                }
+                    do {
+                        linea = "";
+                        try {   
+                            linea = miBufferedReader.readLine();
                             if (linea != null) {
                                 System.out.println(linea);
                             }
-                        } while (linea != null);
-                        break;
-                    default:
-                        System.out.println("Saliendo............");
-                        break;
-                }
-            } while (selectmenu != 3);
-        } catch (FileNotFoundException e) {
-            System.out.println("Error archivo no encoontrado " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("Error no esperado " + e.getMessage());
-        }
+                        } catch (IOException e) {
+                            System.out.println("Error al cerrar el flujo de lectura y escritura " + e.getMessage());
+                        }
+                    } while (linea != null);
+                    break;
+
+                default:
+                    System.out.println("Saliendo............");
+                    break;
+            }
+        } while (selectmenu != 3);
     }
 
 }
